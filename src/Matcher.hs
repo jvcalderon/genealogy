b4208fef2173fcf9819abc64067cab967da011d2
jvcalderon <jvcprod@hotmail.com>
2019-04-27 19:34:04 +0200

    New filter: people just can die once

--- src/Matcher.hs
+++ src/Matcher.hs
@@ -6,9 +6,16 @@ import Data.List.Split
 import Data.UUID.V4
 import Text.Regex.PCRE.Wrap
 
--- Get persons who matches by name or surnames
 getMatches :: Person -> [Person] -> [Person]
-getMatches x = filter $ \p -> name x =~ name p && surnames x =~ surnames p
+getMatches x = justOneDie x . matchesByNameOrSurname x
+
+-- People can't die twice
+justOneDie :: Person -> [Person] -> [Person]
+justOneDie x = filter (\p -> pRole p /= Deceased || pRole x /= Deceased)
+
+-- Get people who matches by name or surnames
+matchesByNameOrSurname :: Person -> [Person] -> [Person]
+matchesByNameOrSurname x xs = filter (\p -> name x =~ name p || surnames x =~ surnames p) (withUid xs)
   where
     regex :: String -> String
     regex = intercalate "|" . splitOn " " . ("(?i)" ++)
@@ -18,3 +25,5 @@ getMatches x = filter $ \p -> name x =~ name p && surnames x =~ surnames p
     name = regex . sanitize . pName
     surnames :: Person -> [Char]
     surnames = regex . sanitize . pSurnames
+    withUid :: [Person] -> [Person]
+    withUid = filter $ \x -> pUid x /= Nothing
--- test/MatcherSpec.hs
+++ test/MatcherSpec.hs
@@ -10,9 +10,9 @@ spec = do
   describe "Match people who can be candidates to be the same person" $ do
     it "[getMatches] Gets persons who matches by name or surname" $ do
       (pName . head)
-        (getMatches
+        (matchesByNameOrSurname
            (Person
-              Nothing
+              (Just $ returnM . fromString $ "4187f6e1-97b5-4cd9-bd48-1a397f78cc55")
               (returnM . fromString $ "4187f6e4-97b5-4cd9-bd48-1a397f78cc55")
               (date "2000-12-12")
               "Antonio Armando"
@@ -20,7 +20,7 @@ spec = do
               "El Mozo"
               Deceased)
            [ Person
-               Nothing
+               (Just $ returnM . fromString $ "4187f6e2-97b5-4cd9-bd48-1a397f78cc55")
                (returnM . fromString $ "4187f6e6-97b5-4cd9-bd48-1a397f78cc55")
                (date "2018-12-12")
                "Arm"
@@ -28,7 +28,7 @@ spec = do
                ""
                Bridegroom
            , Person
-               Nothing
+               (Just $ returnM . fromString $ "4187f6e3-97b5-4cd9-bd48-1a397f78cc55")
                (returnM . fromString $ "4187f6e7-97b5-4cd9-bd48-1a397f78cc55")
                (date "2018-12-12")
                "Pepo"
@@ -37,6 +37,28 @@ spec = do
                Son
            ]) `shouldBe`
         "Arm"
+    it
+      "[justOneDie] People just can die one. Two roles 'Deceased' for same person is impossible. It must be removed from list" $ do
+      length
+        (justOneDie
+           (Person
+              (Just $ returnM . fromString $ "4187f6e1-97b5-4cd9-bd48-1a397f78cc55")
+              (returnM . fromString $ "4187f6e4-97b5-4cd9-bd48-1a397f78cc55")
+              (date "2000-12-12")
+              "Antonio Armando"
+              "Carrascosa Bedulia"
+              "El Mozo"
+              Deceased)
+           [ Person
+               (Just $ returnM . fromString $ "4187f6e2-97b5-4cd9-bd48-1a397f78cc55")
+               (returnM . fromString $ "4187f6e6-97b6-4cd9-bd48-1a397f78cc55")
+               (date "2018-12-12")
+               "Arm"
+               "Ca:r,r. B."
+               ""
+               Deceased
+           ]) `shouldBe`
+        0
   where
     returnM :: Maybe a -> a
     returnM (Just x) = x
