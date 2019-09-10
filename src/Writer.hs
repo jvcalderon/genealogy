5c57bd122915082b6bf55e39c4635ce201f6a7a4
jvcalderon <jvcprod@hotmail.com>
2019-09-10 19:40:14 +0200

    Adds functions to convert Person types in strings to save in documents

--- src/Writer.hs
+++ src/Writer.hs
@@ -1,8 +1,69 @@
 module Writer where
 
 import Data
+import Data.Function (on)
 import Data.List
+import Data.Time
 import Data.UUID
 
 docUids :: [Person] -> [UUID]
 docUids = nub . map pDocUid
+
+returnUid :: Maybe UUID -> String
+returnUid (Just x) = toString x
+returnUid Nothing  = ""
+
+returnDate :: Maybe Day -> String
+returnDate (Just x) = dateStr x
+returnDate Nothing  = ""
+
+strRowFactory :: Person -> String
+strRowFactory x =
+  intercalate
+    ";"
+    [ toString . pDocUid $ x
+    , returnUid . pUid $ x
+    , returnDate . pDate $ x
+    , pName x
+    , pSurnames x
+    , ""
+    , ""
+    , ""
+    , ""
+    , ""
+    , ""
+    , ""
+    ]
+
+getDeathFileContent :: [Person] -> [String]
+getDeathFileContent = map strRowFactory . filter (\x -> pRole x == Deceased)
+
+getBirthsFileContent :: [Person] -> [String]
+getBirthsFileContent xs =
+  map birthFactory $ groupBy (on (==) pDocUid) (filter (\x -> pRole x `elem` [Son, Mother, Father]) xs)
+  where
+    birthFactory :: [Person] -> String
+    birthFactory xs =
+      intercalate
+        ";"
+        [ toString . pDocUid . son $ xs
+        , returnUid . pUid . son $ xs
+        , returnDate . pDate . father $ xs
+        , pName . son $ xs
+        , returnUid . pUid . father $ xs
+        , pName . father $ xs
+        , pSurnames . father $ xs
+        , ""
+        , returnUid . pUid . mother $ xs
+        , pName . mother $ xs
+        , pSurnames . mother $ xs
+        , pNickName . mother $ xs
+        ]
+    getByRole :: Role -> [Person] -> Person
+    getByRole role = head . filter (\x -> pRole x == role)
+    father :: [Person] -> Person
+    father = getByRole Father
+    mother :: [Person] -> Person
+    mother = getByRole Mother
+    son :: [Person] -> Person
+    son = getByRole Son
--- test/WriterSpec.hs
+++ test/WriterSpec.hs
@@ -38,6 +38,90 @@ spec = do
         [ returnM . fromString $ "4187f6e6-97b5-4cd9-bd48-1a397f78cc55"
         , returnM . fromString $ "4187f6e7-97b5-4cd9-bd48-1a397f78cc55"
         ]
+    it "[getDeathFileContent] Should convert a list of people to text filtering by deceased role" $ do
+      getDeathFileContent
+        [ Person
+            (fromString $ "4187f6e2-97b5-4cd9-bd48-1a397f78cc55")
+            (returnM . fromString $ "4187f6e6-97b5-4cd9-bd48-1a397f78cc55")
+            (date "2018-12-12")
+            "Armando"
+            "Caballero"
+            "El Tolillo"
+            Deceased
+        , Person
+            (fromString $ "4187f6e2-97b5-4cd9-bd48-1a397f78cc56")
+            (returnM . fromString $ "4187f6e6-97b5-4cd9-bd48-1a397f78cc56")
+            (date "2018-12-12")
+            "Fernando"
+            "Borreguete"
+            ""
+            Deceased
+        ] `shouldBe`
+        [ "4187f6e6-97b5-4cd9-bd48-1a397f78cc55;4187f6e2-97b5-4cd9-bd48-1a397f78cc55;12/12/2018;Armando;Caballero;;;;;;;"
+        , "4187f6e6-97b5-4cd9-bd48-1a397f78cc56;4187f6e2-97b5-4cd9-bd48-1a397f78cc56;12/12/2018;Fernando;Borreguete;;;;;;;"
+        ]
+    it "[getBirthsFileContent] Should convert a list of people to text filtering and grouping by birth doc" $ do
+      getBirthsFileContent
+        [ Person
+            (fromString $ "4187f6e2-97b5-4cd9-bd48-1a397f78cc55")
+            (returnM . fromString $ "4187f6e6-97b5-4cd9-bd48-1a397f78cc55")
+            (date "2018-12-12")
+            "Armando"
+            "Caballero"
+            "El Tolillo"
+            Deceased
+        , Person
+            (fromString $ "4187f6e2-97b5-4cd9-bd48-1a397f78cc10")
+            (returnM . fromString $ "4187f6e6-97b5-4cd9-bd48-1a397f78cc57")
+            (date "2018-12-12")
+            "Pedro"
+            "Gula"
+            ""
+            Father
+        , Person
+            (fromString $ "4187f6e2-97b5-4cd9-bd48-1a397f78cc11")
+            (returnM . fromString $ "4187f6e6-97b5-4cd9-bd48-1a397f78cc57")
+            (date "2018-12-12")
+            "Maria"
+            "Guti"
+            ""
+            Mother
+        , Person
+            (fromString $ "4187f6e2-97b5-4cd9-bd48-1a397f78cc12")
+            (returnM . fromString $ "4187f6e6-97b5-4cd9-bd48-1a397f78cc57")
+            (date "2018-12-12")
+            "Gimeno"
+            "Gula Guti"
+            ""
+            Son
+        , Person
+            (fromString $ "4187f6e2-97b5-4cd9-bd48-1a397f78cc13")
+            (returnM . fromString $ "4187f6e6-97b5-4cd9-bd48-1a397f78cc60")
+            (date "2018-12-12")
+            "Facundo"
+            "Gracias"
+            ""
+            Father
+        , Person
+            (fromString $ "4187f6e2-97b5-4cd9-bd48-1a397f78cc14")
+            (returnM . fromString $ "4187f6e6-97b5-4cd9-bd48-1a397f78cc60")
+            (date "2018-12-12")
+            "Pepa"
+            "Pelucas"
+            "Peritoria"
+            Mother
+        , Person
+            (fromString $ "4187f6e2-97b5-4cd9-bd48-1a397f78cc15")
+            (returnM . fromString $ "4187f6e6-97b5-4cd9-bd48-1a397f78cc60")
+            (date "2018-12-12")
+            "Juanillo"
+            "Gracias Pelucas"
+            ""
+            Son
+        ] `shouldBe`
+        [ "4187f6e6-97b5-4cd9-bd48-1a397f78cc57;4187f6e2-97b5-4cd9-bd48-1a397f78cc12;12/12/2018;Gimeno;4187f6e2-97b5-4cd9-bd48-1a397f78cc10;Pedro;Gula;;4187f6e2-97b5-4cd9-bd48-1a397f78cc11;Maria;Guti;"
+        , "4187f6e6-97b5-4cd9-bd48-1a397f78cc60;4187f6e2-97b5-4cd9-bd48-1a397f78cc15;12/12/2018;Juanillo;4187f6e2-97b5-4cd9-bd48-1a397f78cc13;Facundo;Gracias;;4187f6e2-97b5-4cd9-bd48-1a397f78cc14;Pepa;Pelucas;Peritoria"
+        ]
   where
     returnM :: Maybe a -> a
     returnM (Just x) = x
